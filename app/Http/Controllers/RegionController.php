bd3fae1d7ee6462562ef556f989d51840640402b
dlutz494 <dlutz494@gmail.com>
2022-09-06 16:30:09 -0400

    Reformat factories

---
+++ app/Http/Controllers/RegionController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Region;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class RegionController extends Controller
+{
+    public function index() : void
+    {
+        //
+    }
+
+    public function create() : void
+    {
+        //
+    }
+
+    public function store(Request $request) : Response
+    {
+
+    }
+
+    public function show(Region $region) : Response
+    {
+        //
+    }
+
+    public function edit(Region $region) : Response
+    {
+        //
+    }
+
+    public function update(Request $request, Region $region) : Response
+    {
+        //
+    }
+
+    public function destroy(Region $region) : Response
+    {
+        //
+    }
+}
