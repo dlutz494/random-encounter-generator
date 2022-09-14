0eda0a5d41aeadf8aa6089ca5d98a91ef230bf3d
dlutz494 <dlutz494@gmail.com>
2022-09-14 14:15:58 -0400

    Region store method returns success or 404 if exception is thrown

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\RegionResource;
 use App\Models\Region;
+use Exception;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -15,14 +16,20 @@ class RegionController extends Controller
         return Region::all();
     }
 
+    public function store(Request $request) : Response
+    {
+        try {
+            Region::create($request->all());
+
+            return new Response('Region stored successfully', 200);
+        } catch (Exception $e) {
+            return new Response('An error occurred', 404);
+        }
+    }
+
     public function create()
     {
         //
-    }
-
-    public function store(Request $request) : Response
-    {
-        return Region::create($request->all());
     }
 
     public function show(Region $region) : RegionResource
--- tests/Feature/RegionController/StoreRegionControllerTest.php
+++ tests/Feature/RegionController/StoreRegionControllerTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature\RegionController;
 
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class StoreRegionControllerTest extends TestCase
 {
@@ -12,12 +12,46 @@ class StoreRegionControllerTest extends TestCase
     public function test_it_stores_a_region_with_name_and_environment_type() : void
     {
         // prepare the parameters for the region to be created
-        $regionName = 'Halcyon Forest';
+        $regionName = 'Store Test';
         $regionType = 'Forest';
+
         // send a request to create a region with a nane and environment type
-        $this->json('POST', 'api/regions', [
+        $response = $this->json('POST', 'api/region', [
             'name'             => $regionName,
             'environment_type' => $regionType,
-        ])->assertSuccessful();
+        ]);
+
+        // assert the request was successful
+        $response->assertSuccessful();
+        // assert the region was added to the database
+        $this->assertDatabaseHas('Regions', [
+            'name'             => $regionName,
+            'environment_type' => $regionType,
+        ]);
     }
+
+    public function test_it_stores_a_region_with_name_and_environment_type_and_parent_region() : void
+    {
+        // prepare the parameters for the region to be created
+        $regionName = 'Halcyon Forest';
+        $regionType = 'Forest';
+        $regionParentName = 'Northern Forests';
+
+        // send a request to create a region with a nane and environment type
+        $response = $this->json('POST', 'api/region', [
+            'name'             => $regionName,
+            'environment_type' => $regionType,
+            'parent_region'    => $regionParentName,
+        ]);
+
+        // assert the request was successful
+        $response->assertSuccessful();
+        // assert the region was added to the database
+        $this->assertDatabaseHas('regions', [
+            'name'             => $regionName,
+            'environment_type' => $regionType,
+            'parent_region'    => $regionParentName,
+        ]);
+    }
+
 }
