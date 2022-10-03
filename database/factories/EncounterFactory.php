7e529c91992820c9489ceb4dbcd74026d09b50de
dlutz494 <dlutz494@gmail.com>
2022-10-03 16:09:13 -0400

    Fix encounter factory's unique name and remove fields

--- database/factories/EncounterFactory.php
+++ database/factories/EncounterFactory.php
@@ -11,10 +11,8 @@ class EncounterFactory extends Factory
     public function definition() : array
     {
         return [
-            'name'        => $this->faker->unique()->title,
+            'name'        => 'Encounter-' . uniqid(),
             'description' => 'Test description',
-            'regions'     => Region::factory()->create(),
-            'enemies'     => Enemy::factory()->create(),
             'difficulty'  => $this->faker->randomElement([
                 'Trivial',
                 'Easy',
@@ -27,20 +25,22 @@ class EncounterFactory extends Factory
 
     public function withRegions(int $count = 1)
     {
-        return $this->state(function () use ($count) {
-            $regions = Region::factory($count)->create();
-
-            return [
-                'regions' => $regions,
-            ];
-        });
+        // Factory Regions then create as many records in the EncounterRegions table
+//        return $this->state(function () use ($count) {
+//            $regions = Region::factory($count)->create();
+//
+//            return [
+//                'regions' => $regions,
+//            ];
+//        });
     }
 
     public function withEnemies(int $count = 1)
     {
-        return $this->state(function () use ($count) {
-            Enemy::factory($count)->create();
-        });
+        // Factory Enemies then create as many records in the EncounterEnemies table
+//        return $this->state(function () use ($count) {
+//            Enemy::factory($count)->create();
+//        });
     }
 
 }
--- tests/Unit/Encounter/EncounterFactoryTest.php
+++ tests/Unit/Encounter/EncounterFactoryTest.php
@@ -2,12 +2,40 @@
 
 namespace Tests\Unit\Encounter;
 
+use App\Models\Encounter;
 use Tests\TestCase;
 
 class EncounterFactoryTest extends TestCase
 {
-    public function test_example() : void
+    public function test_it_creates_an_encounter() : void
     {
-        $this->assertTrue(true);
+        $encounter = Encounter::factory()->create();
+
+        $this->assertNotEmpty($encounter);
+        $this->assertNotEmpty($encounter->name);
+        $this->assertNotEmpty($encounter->description);
+        $this->assertNotEmpty($encounter->difficulty);
     }
+
+    public function test_it_creates_an_encounter_with_many_regions() : void
+    {
+        $this->markTestSkipped('Need to add region functionality in Encounter factory');
+        $count = 5;
+        $encounter = Encounter::factory()->withManyRegions($count)->create();
+
+        $this->assertNotEmpty($encounter);
+        $this->assertCount($count, $encounter->regions);
+    }
+
+    public function test_it_creates_an_encounter_with_many_enemies() : void
+    {
+        $this->markTestSkipped('Need to add enemy functionality in Encounter factory');
+        $count = 5;
+        /** @var Encounter $encounter */
+        $encounter = Encounter::factory()->withManyEnemies($count)->create();
+
+        $this->assertNotEmpty($encounter);
+        $this->assertCount($count, $encounter->enemies);
+    }
+
 }
