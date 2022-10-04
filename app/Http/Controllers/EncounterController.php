19fac02054e15d5fc73639f7f54016fd8d51949c
dlutz494 <dlutz494@gmail.com>
2022-10-03 23:24:44 -0400

    Add empty controller, policy, seeder, and store and update requests for Encounter model

---
+++ app/Http/Controllers/EncounterController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEncounterRequest;
+use App\Http\Requests\UpdateEncounterRequest;
+use App\Models\Encounter;
+
+class EncounterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreEncounterRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreEncounterRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Encounter  $encounter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Encounter $encounter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Encounter  $encounter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Encounter $encounter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateEncounterRequest  $request
+     * @param  \App\Models\Encounter  $encounter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateEncounterRequest $request, Encounter $encounter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Encounter  $encounter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Encounter $encounter)
+    {
+        //
+    }
+}
