736cec07724596c5834cd0999a3d0565eb29f4dd
dlutz494 <dlutz494@gmail.com>
2022-10-07 00:59:39 -0400

    Add regions and enemies fillable fields to encounter model

--- app/Models/Encounter.php
+++ app/Models/Encounter.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -10,9 +11,11 @@ class Encounter extends Model
 {
     use HasFactory;
 
-    protected mixed $name;
-    protected mixed $description;
-    protected mixed $difficulty;
+    protected string $name;
+    protected string $description;
+    protected string $difficulty;
+    protected mixed $regions;
+    protected mixed $enemies;
 
     protected $table = 'encounters';
 
@@ -20,6 +23,8 @@ class Encounter extends Model
         'name',
         'description',
         'difficulty',
+        'regions',
+        'enemies',
     ];
 
     public function regions() : BelongsToMany
