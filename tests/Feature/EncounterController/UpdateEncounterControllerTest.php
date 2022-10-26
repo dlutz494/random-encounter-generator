9cb3b3666145bbc429737685cc43de9cff3dc7d9
dlutz494 <dlutz494@gmail.com>
2022-10-26 12:50:38 -0400

    Add encounter update tests

---
+++ tests/Feature/EncounterController/UpdateEncounterControllerTest.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Tests\Feature\EncounterController;
+
+use App\Models\Encounter;
+use App\Models\EncounterEnemy;
+use App\Models\EncounterRegion;
+use App\Models\Enemy;
+use App\Models\Region;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\TestResponse;
+use Tests\TestCase;
+
+class UpdateEncounterControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected Encounter $encounter;
+
+    public function test_it_updates_an_encounter() : void
+    {
+        $name = 'Update Test ' . uniqid();
+        $regions = [Region::factory()->create()->getKey()];
+        $enemies = [Enemy::factory()->create()->getKey()];
+        $payload = [
+            'name'        => $name,
+            'description' => 'A test encounter',
+            'difficulty'  => 'Trivial',
+            'regions'     => $regions,
+            'enemies'     => $enemies,
+        ];
+
+        $response = $this->getResponse($payload);
+
+        $response->assertSuccessful();
+        $this->assertDatabaseHas('encounters', [
+            'name'        => $name,
+            'description' => 'A test encounter',
+            'difficulty'  => 'Trivial',
+        ]);
+        $this->assertDatabaseHas('encounter_region', [
+            'region_id' => $regions[0],
+        ]);
+        $this->assertDatabaseHas('encounter_enemy', [
+            'enemy_id' => $enemies[0],
+        ]);
+    }
+
+    public function getResponse(array $payload) : TestResponse
+    {
+        return $this->json('PUT', 'api/encounter/' . $this->encounter->getKey(), $payload);
+    }
+
+    public function test_it_returns_unprocessable_with_duplicate_name() : void
+    {
+        $name = 'Update Test ' . uniqid();
+        Encounter::factory()->create([
+            'name' => $name,
+        ]);
+
+        $response = $this->getResponse(['name' => $name]);
+
+        $response->assertUnprocessable();
+        $response->assertSee('The name has already been taken.');
+    }
+
+    /**
+     * @dataProvider ProvidesValidPayloads
+     */
+    public function test_it_returns_success_with_valid_payloads($payload) : void
+    {
+        $response = $this->getResponse($payload);
+
+        $response->assertSuccessful();
+    }
+
+    /**
+     * @dataProvider ProvidesInvalidPayloads
+     */
+    public function test_it_returns_unprocessable_with_invalid_payloads($payload, $errors) : void
+    {
+        $response = $this->getResponse($payload);
+
+        $response->assertUnprocessable()->assertSee($errors);
+    }
+
+    public function ProvidesValidPayloads() : array
+    {
+        $name = 'Update encounter';
+        $description = 'A test encounter';
+        $difficulty = 'Trivial';
+        $regions = [];
+        $enemies = [];
+
+        return [
+            'Name only'        => [
+                'payload' => [
+                    'name' => $name,
+                ],
+            ],
+            'Description only' => [
+                'payload' => [
+                    'description' => $description,
+                ],
+            ],
+            'Difficulty only'  => [
+                'payload' => [
+                    'difficulty' => $difficulty,
+                ],
+            ],
+            'Regions only'     => [
+                'payload' => [
+                    'regions' => $regions,
+                ],
+            ],
+            'Enemies only'     => [
+                'payload' => [
+                    'enemies' => $enemies,
+                ],
+            ],
+            'All fields'       => [
+                'payload' => [
+                    'name'        => $name,
+                    'description' => $description,
+                    'difficulty'  => $difficulty,
+                    'regions'     => $regions,
+                    'enemies'     => $enemies,
+                ],
+            ],
+        ];
+    }
+
+    public function ProvidesInvalidPayloads() : array
+    {
+        $validName = 'Update Encounter';
+        $validDescription = 'A test encounter';
+        $validDifficulty = 'Trivial';
+        $validRegions = [];
+        $validEnemies = [];
+        return [
+            'Name is an integer'        => [
+                'payload' => [
+                    'name' => 123,
+                ],
+                'errors'  => [
+                    'The name must be a string',
+                ],
+            ],
+            'Description is an integer' => [
+                'payload' => [
+                    'description' => 123,
+                ],
+                'errors'  => [
+                    'The description must be a string',
+                ],
+            ],
+            'Difficulty is an integer'  => [
+                'payload' => [
+                    'difficulty' => 123,
+                ],
+                'errors'  => [
+                    'The difficulty must be a string',
+                ],
+            ],
+            'Regions is an integer'     => [
+                'payload' => [
+                    'regions' => 123,
+                ],
+                'errors'  => [
+                    'The regions must be an array',
+                ],
+            ],
+            'Enemies is an integer'     => [
+                'payload' => [
+                    'enemies' => 123,
+                ],
+                'errors'  => [
+                    'The enemies must be an array',
+                ],
+            ],
+            'All fields are invalid'    => [
+                'payload' => [
+                    'name'        => 123,
+                    'description' => 123,
+                    'difficulty'  => 123,
+                    'regions'     => 123,
+                    'enemies'     => 123,
+                ],
+                'errors'  => [
+                    'The name must be a string. (and 4 more errors)',
+                ],
+            ],
+        ];
+    }
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+
+        $this->encounter = Encounter::factory()->create();
+        EncounterRegion::factory()->withEncounter($this->encounter)->create();
+        EncounterEnemy::factory()->withEncounter($this->encounter)->create();
+    }
+
+}
