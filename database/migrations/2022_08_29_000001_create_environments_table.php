cc47eb3b967478e9c611dec73d571ded21b69736
dlutz494 <dlutz494@gmail.com>
2022-10-26 12:47:32 -0400

    Add default environment name in migration

--- database/migrations/2022_08_29_000001_create_environments_table.php
+++ database/migrations/2022_08_29_000001_create_environments_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('environments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default('Environment');
             $table->timestamps();
         });
     }
