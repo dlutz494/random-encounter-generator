429b70bf8a3afbc73241186dda14cac7d909dd0c
dlutz494 <dlutz494@gmail.com>
2022-11-11 16:43:52 -0500

    Add create and edit pages

---
+++ resources/views/environment/edit.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Environment Edit</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <h1>Edit</h1>
+        <form>
+            <label for="name"></label>
+            <input id="name" type="text" value="{{ $environment->name }}">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
