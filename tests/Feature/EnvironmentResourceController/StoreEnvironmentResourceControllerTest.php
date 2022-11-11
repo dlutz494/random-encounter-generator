affb168d04c98881f5dc27d9c78a258f0f7a58b4
dlutz494 <dlutz494@gmail.com>
2022-11-11 16:43:34 -0500

    Fix namespaces

--- tests/Feature/EnvironmentResourceController/StoreEnvironmentResourceControllerTest.php
+++ tests/Feature/EnvironmentResourceController/StoreEnvironmentResourceControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\EnvironmentController;
+namespace Tests\Feature\EnvironmentResourceController;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
