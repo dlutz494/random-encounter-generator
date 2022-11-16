a636176140beba0edd95d301fc812c36d56f6c7e
dlutz494 <dlutz494@gmail.com>
2022-11-16 17:04:23 -0500

    Reorder routes

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,20 @@ Route::get('/', function () {
 
 Route::controller(EnvironmentController::class)->group(function () {
     Route::get('/environment', 'index');
-    Route::post('/environment', 'store');
     Route::get('/environment/create', 'create');
     Route::get('/environment/{environment}', 'show');
     Route::get('/environment/{environment}/edit', 'edit');
+    Route::post('/environment', 'store');
     Route::patch('/environment/{environment}/update', 'update');
     Route::delete('/environment/{environment}/delete', 'destroy');
 });
 
 Route::controller(RegionController::class)->group(function () {
     Route::get('/region', 'index');
-    Route::post('/region', 'store');
     Route::get('/region/create', 'create');
     Route::get('/region/{region}', 'show');
     Route::get('/region/{region}/edit', 'edit');
+    Route::post('/region', 'store');
     Route::patch('/region/{region}/update', 'update');
     Route::delete('/region/{region}/delete', 'destroy');
 });
